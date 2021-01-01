783bdcd57546f1c233ccb787c1dbf240b89b3057
Ahmet Uyar <4hmetuyar@gmail.com>
2021-01-01 18:17:27 +0300

    Example: How to use where?

--- Example.LinqQuery/Program.cs
+++ Example.LinqQuery/Program.cs
@@ -5,6 +5,35 @@ namespace Example.LinqQuery
 {
     class Program
     {
+     
+        static void Main(string[] args)
+        {
+            
+
+            ExampleOne();
+        }
+
+
+        /// <summary>
+        /// Where example
+        /// </summary>
+        private static void ExampleOne()
+        {
+            var list = new int[] { 1, 3, 33, 44, 56, 101 };
+ 
+            var oddNumbers =
+                from number in list
+                where number % 2 == 1
+                select number;
+ 
+            foreach (var value in oddNumbers)   
+            {
+                Console.WriteLine(value);
+            }
+ 
+            Console.ReadKey();
+        }
+        
         class Plant
         {
             public string Name { get; set; }
@@ -14,26 +43,31 @@ namespace Example.LinqQuery
         {
             public string TrapType { get; set; }
         }
+
         
-        static void Main(string[] args)
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void ExampleTwo()
         {
-              Plant[] plants = new Plant[] {
-                    new CarnivorousPlant { Name = "Venus Fly Trap", TrapType = "Snap Trap" },
-                    new CarnivorousPlant { Name = "Pitcher Plant", TrapType = "Pitfall Trap" },
-                    new CarnivorousPlant { Name = "Sundew", TrapType = "Flypaper Trap" },
-                    new CarnivorousPlant { Name = "Waterwheel Plant", TrapType = "Snap Trap" }
-                };
+        Plant[] plants = new Plant[] {
+            new CarnivorousPlant { Name = "Venus Fly Trap", TrapType = "Snap Trap" },
+            new CarnivorousPlant { Name = "Pitcher Plant", TrapType = "Pitfall Trap" },
+            new CarnivorousPlant { Name = "Sundew", TrapType = "Flypaper Trap" },
+            new CarnivorousPlant { Name = "Waterwheel Plant", TrapType = "Snap Trap" }
+        };
             
-                var query = from CarnivorousPlant cPlant in plants
-                            where cPlant.TrapType == "Snap Trap"
-                            select cPlant;
-            
-                foreach (Plant plant in query)
-                    Console.WriteLine(plant.Name);
-            
-            Console.WriteLine("Hello World!");
+        var query = from CarnivorousPlant cPlant in plants
+                where cPlant.TrapType == "Snap Trap"
+                select cPlant;
+
+        foreach (Plant plant in query)
+        {
+            Console.WriteLine(plant.Name);
         }
-        
+             
+
+        }
         
     }
 }
